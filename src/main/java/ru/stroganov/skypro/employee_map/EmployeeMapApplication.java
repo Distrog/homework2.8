4058cee5c1a9fd87b481039c2382b30ac7085769
Dmitriy Stroganov <Distrog@gmail.com>
2024-03-18 20:06:25 +0700

    Inititial commit

---
+++ src/main/java/ru/stroganov/skypro/employee_map/EmployeeMapApplication.java
@@ -0,0 +1,13 @@
+package ru.stroganov.skypro.employee_map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/stroganov/skypro/employee_map/EmployeeMapApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.stroganov.skypro.employee_map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
